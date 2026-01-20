efce875368f4e89db4d04f469305b79be564a532
Matus Ivanecky <maty535@users.noreply.github.com>
2026-01-20 19:30:36 +0100

    Update tazbaSearch.js

--- assets/js/tazbaSearch.js
+++ assets/js/tazbaSearch.js
@@ -4,13 +4,27 @@ function search(){
   var s = document.getElementById('search').value.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '')
 
   rows = (document.getElementsByTagName('tbody')[1]).getElementsByTagName('TR');
-  for(var i=0;i<rows.length;i++){
-    if(rows[i].textContent.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '').indexOf(s)>0  || s.length==0 ) {
+
+  var pocet = 0;
+  var objem = 0.0;
+	
+  for(var i=0; i < rows.length; i++){
+	  
+    if(rows[i].textContent.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '').indexOf(s) > 0  || s.length==0 ) {
 	  rows[i].style.display ='';
+	  pocet++;
+	   // Index 1 je 7 stĺpec (objem vyrezu)
+	  var cellVal =  rows[i].cells[6].textContent
+      objem += parseFloat(cellVal);
+      
+		  
     } else {
       rows[i].style.display ='none';
     }
   }
+
+  // Zapíšeme výsledok do bunky v tfoot
+  document.getElementById('summaryInfo').textContent = objem.toFixed(2) + " m³ / (" + pocet + "ks )";
 }
 
 
