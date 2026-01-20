8ca90c1d5434afa0c7209d6d3026f9d23dada072
Matus Ivanecky <maty535@users.noreply.github.com>
2026-01-20 19:13:09 +0100

    Create tazbaSearch.js

---
+++ assets/js/tazbaSearch.js
@@ -0,0 +1,25 @@
+
+function search(){
+  
+  var s = document.getElementById('search').value.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+
+  rows = (document.getElementsByTagName('tbody')[1]).getElementsByTagName('TR');
+  for(var i=0;i<rows.length;i++){
+    if(rows[i].textContent.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '').indexOf(s)>0  || s.length==0 ) {
+	  rows[i].style.display ='';
+    } else {
+      rows[i].style.display ='none';
+    }
+  }
+}
+
+
+var timer;
+function delayedSearch() {
+	clearTimeout(timer);
+	console.log('delay-ing')
+    timer = setTimeout(function () {
+		console.log('delay-running')
+		search();
+    }, 500);
+  }
